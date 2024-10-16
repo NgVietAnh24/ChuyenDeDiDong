82364b2b1160cbfaa55a36d0040ae52f2bcaa4a0
VietAnh244 <117790397+NgVietAnh24@users.noreply.github.com>
2024-10-16 21:58:00 +0700

    Hoàn thiện chức năng đăng nhập và Quên mật khẩu Đăng ký người dùng mới đang xử lý

---
+++ app/src/main/java/vn/posicode/chuyende/QLNhanVien.java
@@ -0,0 +1,15 @@
+package vn.posicode.chuyende;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QLNhanVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ql_nhanvien_layout);
+    }
+}
